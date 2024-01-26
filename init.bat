aec3028c58de5a3a414f2fa6fffc5136711d4136
MauricioDMO <mauriciodmoprogrammer@gmail.com>
2024-01-25 21:37:16 -0600

    Refactor init.bat script to improve readability and provide clearer messages

--- init.bat
+++ init.bat
@@ -1,18 +1,18 @@
 @echo off
 
 if exist "env" (
-    echo carpeta "env" existe
-    echo "Activando entorno virtual"
+    echo "env" existe
+    echo Activando entorno virtual
     env\Scripts\activate
 ) else (
-    echo "env" folder does not exist
-    echo "Creando entorno virtual"
-    python -m venv env
-    echo "Activando entorno virtual"
+    echo "env" no existe
+    echo Creando entorno virtual
+    py -m venv env
+    echo Activando entorno virtual
     env\Scripts\activate
-    echo "Instalando dependencias"
+    echo Instalando dependencias
     pip install -r requirements.txt
-    echo "Done"
+    echo Entorno virtual creado y activado
 )
 
 echo.
