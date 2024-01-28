2aca4d15d28f356875d03a6b20e09594232a3c3d
MauricioDMO <mauriciodmoprogrammer@gmail.com>
2024-01-28 15:25:09 -0600

    Moving main.py to the root and adding main function

---
+++ main.py
@@ -0,0 +1,22 @@
+import cv2, json, numpy as np, sqlite3 as sql
+
+def main():
+  image = cv2.imread("main2.jpg", cv2.IMREAD_GRAYSCALE)
+
+  list = image.tolist()
+  string = json.dumps(list)
+
+  list2 = json.loads(string)
+  image2 = np.array(list2, dtype=np.uint8)
+
+  cv2.imshow("Image", image2)
+  cv2.waitKey(0)
+  cv2.destroyAllWindows()
+
+
+  # cv2.imshow("Image", image)
+  # cv2.waitKey(0)
+  # cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+  main()
