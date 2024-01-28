3641dd265ae46229bb01e8cbb2d2b850b7ccb2ed
MauricioDMO <mauriciodmoprogrammer@gmail.com>
2024-01-28 15:33:21 -0600

    Add load function documentation

--- src/people.py
+++ src/people.py
@@ -1,6 +1,12 @@
 import sqlite3 as sql
 
 def load():
+  """
+  Load all the records from the 'people' table in the 'face-recognition.db' database.
+
+  Returns:
+    A list of tuples representing the records from the 'people' table.
+  """
   CONNECTION = sql.connect("face-recognition.db")
   cursor = CONNECTION.cursor()
   response = cursor.execute("SELECT * FROM people")
