cdfbf2282f3c4f3746bd60789dd5b1049cfa51e9
MauricioDMO <mauriciodmoprogrammer@gmail.com>
2024-01-29 10:13:59 -0600

    Add function to add a person to the database

--- src/people.py
+++ src/people.py
@@ -16,3 +16,34 @@ def load():
   CONNECTION.close()
   return list
 
+
+def addPerson(name: str, lastname: str, carnet: str, image: str, role_id: str):
+  """
+  Add a person to the database.
+  Submit data with object destructuring.
+
+  Args:
+    name (str): The person's first name.
+    lastname (str): The person's last name.
+    carnet (str): The person's identification number (optional).
+    image (str): The path to the person's image.
+    role_id (int): The person's id role.
+
+  Returns:
+    None
+  """
+  
+  with open(image, "rb") as img:
+    image = img.read()
+  
+  CONNECTION = sql.connect("face-recognition.db")
+  
+  cursor = CONNECTION.cursor()
+  
+  cursor.execute("""INSERT INTO people
+                 (name, lastname, carnet, image, role_id, state_id)
+                 VALUES
+                 (?, ?, ?, ?, ?, 1)""", (name, lastname, carnet, image, role_id))
+  CONNECTION.commit()
+  CONNECTION.close()
+  
