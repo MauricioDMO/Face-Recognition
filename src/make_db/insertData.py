25ccf87acc73959c2da1aa57b93b445b35857de0
MauricioDMO <mauriciodmoprogrammer@gmail.com>
2024-01-29 11:06:21 -0600

    Add classrooms to the database

--- src/make_db/insertData.py
+++ src/make_db/insertData.py
@@ -25,3 +25,21 @@ def init():
                         """)
   CONNECTION.commit()
   
+  classrooms = [
+    ("21-22", 1),
+    ("23-24", 1),
+    ("25-26", 1),
+    ("27-28", 1),
+    ("31-32", 1),
+    ("33-34", 1),
+    ("35-36", 1),
+    ("37-38", 1),
+    ("41-42", 1),
+    ("43-44", 1),
+    ("45-46", 1),
+    ("47-48", 1)
+  ]
+  
+  #? insert classrooms into the table classrooms.
+  cursor.execute("""INSERT INTO classrooms (classroom, building_id) values (?, ?)""", classrooms)
+  
